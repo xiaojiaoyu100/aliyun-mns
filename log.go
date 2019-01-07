979bb9afee37d61cffa0a7eb2ca24d7a26f6ed82
zenghongru <zenghongru@gmail.com>
2019-01-07 10:10:57 +0800

    feat: 去掉logrus pretty print

--- log.go
+++ log.go
@@ -16,7 +16,7 @@ var contextLogger = log.WithFields(log.Fields{
 func init() {
 	log.SetFormatter(&log.JSONFormatter{
 		TimestampFormat: "2006-01-02 15:04:05",
-		PrettyPrint:     true})
+	})
 	log.SetReportCaller(true)
 	log.SetOutput(os.Stderr)
 	log.SetLevel(log.InfoLevel)
