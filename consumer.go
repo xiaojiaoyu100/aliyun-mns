1528c49e72a75a336d5f2421c8f27cad6c0c9d05
zenghur <zenghongru@gmail.com>
2020-02-19 17:54:41 -0800

    feat: receipt handle

--- consumer.go
+++ consumer.go
@@ -413,6 +413,7 @@ func (c *Consumer) OnReceive(queue *Queue, receiveMsg *ReceiveMessage) {
 		m.MessageBody = body
 		m.EnqueueTime = receiveMsg.EnqueueTime
 		m.codec = queue.codec
+		m.ReceiptHandle = receiveMsg.ReceiptHandle
 		errChan <- queue.OnReceive(queue.makeContext(m), m)
 	}()
 
--- queue.go
+++ queue.go
@@ -7,10 +7,11 @@ import (
 
 // M 消息内容，去掉其它字段是为了不要依赖消息其它字段，应该依赖数据库字段
 type M struct {
-	QueueName   string // 队列名
-	MessageBody string // 消息体
-	EnqueueTime int64  // 入队时间
-	codec       Codec
+	QueueName     string // 队列名
+	MessageBody   string // 消息体
+	EnqueueTime   int64  // 入队时间
+	ReceiptHandle string // 消息句柄
+	codec         Codec
 }
 
 // Decode 解析消息
