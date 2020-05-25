4c909eee0da92e704a4669a9e9dae571523456fb
tantiantang <tantiantang@xiao100.com>
2020-05-25 17:55:44 +0800

    style(log): 语法注释检查

--- log.go
+++ log.go
@@ -9,7 +9,7 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-// zapcore.Entry + err.error() + []zap.Fields
+// Entry 在zapcore.Entry基础上增加 error & []zap.Fields
 type Entry struct {
 	Level      zapcore.Level `json:"-"`
 	LevelStr   string        `json:"level"`
@@ -18,11 +18,12 @@ type Entry struct {
 	Caller     string        `json:"caller"`
 	Stack      string        `json:"stack"`
 	Fields     []zap.Field   `json:"-"` // 不包含error字段
-	FieldsJson string        `json:"-"`
+	FieldsJSON string        `json:"-"`
 	Err        error         `json:"-"`
 	ErrStr     string        `json:"error"`
 }
 
+// NewLogger 创建一个logger
 func NewLogger() (logger *zap.Logger, err error) {
 	conf := zap.NewProductionConfig()
 	conf.EncoderConfig = newEncoderConfig()
@@ -30,12 +31,14 @@ func NewLogger() (logger *zap.Logger, err error) {
 	return
 }
 
+// Hooks 回调钩子(warn级别以上回调)
 func Hooks(hooks ...func(entry Entry) error) zap.Option {
 	return zap.WrapCore(func(core zapcore.Core) zapcore.Core {
 		return registerHooksWithErrField(core, hooks...)
 	})
 }
 
+// DefaultHook 提供一个默认钩子
 func DefaultHook(callback func(entryJson string, fieldsJson string)) func(entry Entry) error {
 	return func(en Entry) error {
 		if en.Level < zapcore.WarnLevel {
@@ -45,12 +48,13 @@ func DefaultHook(callback func(entryJson string, fieldsJson string)) func(entry 
 		if err != nil {
 			return err
 		}
-		callback(string(b), en.FieldsJson)
+		callback(string(b), en.FieldsJSON)
 		return nil
 	}
 
 }
 
+// NewEntryFromZapEntry zapcore.Entry转换到Entry
 func NewEntryFromZapEntry(ent zapcore.Entry) Entry {
 	return Entry{
 		Level:      ent.Level,
@@ -107,7 +111,7 @@ func (h *hookedWithErrField) Write(ent zapcore.Entry, fs []zapcore.Field) error 
 	}
 
 	buf, _ := h.enc.EncodeEntry(ent, entry.Fields)
-	entry.FieldsJson = buf.String()
+	entry.FieldsJSON = buf.String()
 	buf.Free()
 	for i := range h.funcs {
 		err = multierr.Append(err, h.funcs[i](entry))
